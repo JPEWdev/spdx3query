3777fa8705d51f58d6e00c93ae8db99f6c192adc
Joshua Watt <JPEWhacker@gmail.com>
2024-04-17 12:09:20 -0500

    tests: Invoke as module
    Adds a test to invoke spdx3query as a module

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,7 +3,12 @@
 # SPDX-License-Identifier: MIT
 
 import subprocess
+import sys
 
 
 def test_help():
     subprocess.run(["spdx3query", "--help"], check=True)
+
+
+def test_module():
+    subprocess.run([sys.executable, "-m", "spdx3query", "--help"], check=True)
